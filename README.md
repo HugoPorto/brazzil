68c02df8bf1643afcf3f851f9ebd1b8f0ad2fb8b
HugoPorto <victor.almeida.porto7@gmail.com>
2022-05-08 06:49:18 -0300

    Update no Readme, Remoção de arquivos desnecessários do webroot e correção no controller do carrinho.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# README #
+# [Brazzil Commerce - CMS](https://brazzilcommerce.com/)
 
-Em Desenvolvimento
+**Brazzil Commerce CMS - PT-BR** O Brazzil Commerce é uma ferramenta para criar lojas vituais, ele foi pensado para trabalhar de forma enxuta.
+Com o mínimo de configuração possível, e já trazendo integração com um meio de pagamento pré-configurado e funcional.
+
+**Brazzil Commerce CMS - ES** Brazzil Commerce is a tool to create virtual stores, it was designed to work in a lean way.
+With as little configuration as possible, and already bringing integration with a pre-configured and functional means of payment.
--- src/Controller/StoresCartsController.php
+++ src/Controller/StoresCartsController.php
@@ -14,29 +14,25 @@ class StoresCartsController extends AppController
 
     public function index()
     {
-        if ($this->Roles->get($this->Auth->user()['roles_id'])->role == 'root') {
-            $this->paginate = [
-                'contain' => ['StoresProducts', 'Users']
-            ];
-            $storesCarts = $this->paginate($this->StoresCarts);
+        $this->hasPermission('store');
 
-            $this->set(compact('storesCarts'));
-        } else {
-            $this->redirectSignup();
-        }
+        $this->paginate = [
+            'contain' => ['StoresProducts', 'Users']
+        ];
+        $storesCarts = $this->paginate($this->StoresCarts);
+
+        $this->set(compact('storesCarts'));
     }
 
     public function view($id = null)
     {
-        if ($this->Roles->get($this->Auth->user()['roles_id'])->role == 'root') {
-            $storesCart = $this->StoresCarts->get($id, [
-                'contain' => ['StoresProducts', 'Users']
-            ]);
+        $this->hasPermission('store');
 
-            $this->set('storesCart', $storesCart);
-        } else {
-            $this->redirectSignup();
-        }
+        $storesCart = $this->StoresCarts->get($id, [
+            'contain' => ['StoresProducts', 'Users']
+        ]);
+
+        $this->set('storesCart', $storesCart);
     }
 
     private function validateQuantity($request)
@@ -51,68 +47,43 @@ class StoresCartsController extends AppController
 
     public function add()
     {
-        if ($this->Auth->user() !== null && $this->Roles->get($this->Auth->user()['roles_id'])->role === 'store') {
-                $storesCart = $this->StoresCarts->newEntity();
+        $this->hasPermission('store');
 
-            if ($this->request->is('post')) {
-                $storesCart = $this->StoresCarts->patchEntity($storesCart, $this->request->getData());
+        $storesCart = $this->StoresCarts->newEntity();
 
-                if ($this->validateQuantity($this->request->getData())) {
-                    if ($this->StoresCarts->save($storesCart)) {
-                        return $this->redirect(['controller' => 'homes', 'action' => 'storeCart']);
-                    }
+        if ($this->request->is('post')) {
+            $storesCart = $this->StoresCarts->patchEntity($storesCart, $this->request->getData());
 
-                    $this->Flash->error(__('Erro ao adicionar item ao carrinho.'));
-                    $this->redirectReferer();
-                } else {
-                    $this->redirectReferer();
-                }
-            }
-
-
-            $this->redirectReferer();
-        } else {
-            $this->redirectSignup();
-        }
-    }
-
-    public function edit($id = null)
-    {
-        if ($this->Roles->get($this->Auth->user()['roles_id'])->role == 'root') {
-            $storesCart = $this->StoresCarts->get($id, [
-                'contain' => []
-            ]);
-            if ($this->request->is(['patch', 'post', 'put'])) {
-                $storesCart = $this->StoresCarts->patchEntity($storesCart, $this->request->getData());
+            if ($this->validateQuantity($this->request->getData())) {
                 if ($this->StoresCarts->save($storesCart)) {
-                    $this->Flash->success(__('The stores cart has been saved.'));
-
-                    return $this->redirect(['action' => 'index']);
+                    return $this->redirect(['controller' => 'homes', 'action' => 'storeCart']);
                 }
-                $this->Flash->error(__('The stores cart could not be saved. Please, try again.'));
+
+                $this->Flash->error(__('Erro ao adicionar item ao carrinho.'));
+                $this->redirectReferer();
+            } else {
+                $this->redirectReferer();
             }
-            $storesProducts = $this->StoresCarts->StoresProducts->find('list', ['limit' => 200]);
-            $users = $this->StoresCarts->Users->find('list', ['limit' => 200]);
-            $this->set(compact('storesCart', 'storesProducts', 'users'));
-        } else {
-            $this->redirectSignup();
         }
+
+
+        $this->redirectReferer();
     }
 
     public function delete($id = null)
     {
-        if ($this->Roles->get($this->Auth->user()['roles_id'])->role == 'root') {
-            $this->request->allowMethod(['post', 'delete']);
-            $storesCart = $this->StoresCarts->get($id);
-            if ($this->StoresCarts->delete($storesCart)) {
-                $this->Flash->success(__('The stores cart has been deleted.'));
-            } else {
-                $this->Flash->error(__('The stores cart could not be deleted. Please, try again.'));
-            }
+        $this->hasPermission('store');
 
-            return $this->redirect(['action' => 'index']);
+        $this->request->allowMethod(['post', 'delete']);
+
+        $storesCart = $this->StoresCarts->get($id);
+
+        if ($this->StoresCarts->delete($storesCart)) {
+            $this->Flash->success(__('The stores cart has been deleted.'));
         } else {
-            $this->redirectSignup();
+            $this->Flash->error(__('The stores cart could not be deleted. Please, try again.'));
         }
+
+        return $this->redirect(['action' => 'index']);
     }
 }
